9edbf1b4d61153635328ca85d159c5a3e441daca
philip.belousov <belousovfilip@gmail.com>
2020-04-09 14:00:14 +0700

    skb3.3 - migrations

---
+++ database/migrations/2020_04_06_115012_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->string('slug', 150);
+            $table->string('desc_short', 300);
+            $table->text('desc_full');
+            $table->string('status', 150);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
