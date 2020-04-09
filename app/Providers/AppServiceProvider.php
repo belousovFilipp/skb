9fd09dffdf080333f601e71aa1f55d2db18e6849
philip.belousov <belousovfilip@gmail.com>
2020-04-09 14:27:40 +0700

    skb3.3 Update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\ApiResponse\Responses\JsonApiResponse;
+use App\Services\Posts\Repositories\EloquentPostRepository;
+use App\Services\Posts\Repositories\PostRepositoryInterface;
+use App\Services\ApiResponse\Responses\ApiResponseInterface;
+use App\Services\Feedbacks\Repositories\EloquentFeedbackRepository;
+use App\Services\Feedbacks\Repositories\FeedbackRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(ApiResponseInterface::class, JsonApiResponse::class);
+        $this->app->bind(PostRepositoryInterface::class, EloquentPostRepository::class);
+        $this->app->bind(FeedbackRepositoryInterface::class, EloquentFeedbackRepository::class);
     }
 }
