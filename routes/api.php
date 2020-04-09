513341389a3df0f3797149eef2cae41747d84f44
philip.belousov <belousovfilip@gmail.com>
2020-04-09 14:20:37 +0700

    skb3.3 created routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::name('api.')->group(function () {
+    //CLIENT
+    Route::group(['namespace' => 'Client\Api'], function () {
+        //POSTS
+        Route::apiResource('posts', 'Posts\PostController')
+            ->only(['index', 'show', 'store']);
+        //FEEDBACKS
+        Route::apiResource('feedbacks', 'Feedbacks\FeedbackController')
+            ->only(['store']);
+    });
+
+    //ADMIN
+    Route::group(['namespace' => 'Admin\Api'], function () {
+        //FEEDBACKS
+        Route::apiResource('admins/feedbacks', 'Feedbacks\FeedbackController')
+            ->only(['index']);
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,45 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//---CLIENT---
+Route::group(['namespace' => 'Client'], function () {
+    //PAGES
+    Route::group(['namespace' => 'Pages'], function () {
+        //HOME
+        Route::get('/', 'HomeController')
+            ->name('home');
+        //HOME
+        Route::get('/about', 'AboutController')
+            ->name('about');
+        //CONTACTS
+        Route::get('/contacts', 'ContactsController')
+            ->name('contacts');
+    });
 
-Route::get('/', function () {
-    return view('welcome');
+    //CMS
+    Route::group(['namespace' => 'Cms'], function () {
+        //POSTS
+        Route::resource('posts', 'Posts\PostController')
+            ->only(['show', 'create', 'store']);
+
+        //FEEDBACKS
+        Route::resource('feedbacks', 'Feedbacks\FeedbackController')
+            ->only(['store']);
+    });
 });
+
+//---ADMIN---
+Route::group(['namespace' => 'Admin'], function () {
+    //HOME
+    Route::get('/admins', 'Pages\HomeController')
+        ->name('admin.home');
+
+    Route::name('admins.')->group(function () {
+        //FEEDBACKS
+        Route::resource('/feedbacks', 'Cms\FeedbackController')
+            ->only(['index']);
+    });
+});
+
+
+
